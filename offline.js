dd81582b03a5ee836abc2724deb93a2f27555e6d
Ivan3424 <wwj3424v@163.com>
2018-07-05 21:09:38 +0800

    modify

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1530796142,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/music-sheet0.png",
+		"images/music-sheet1.png",
+		"images/ball-sheet0.png",
+		"images/line-sheet0.png",
+		"images/arrow-sheet0.png",
+		"images/circle2-sheet0.png",
+		"images/bg_game.png",
+		"images/sprite4-sheet0.png",
+		"images/title-sheet0.png",
+		"images/start-sheet0.png",
+		"images/turto-sheet0.png",
+		"images/bg_turto.png",
+		"images/rank-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/again-sheet0.png",
+		"images/score.png",
+		"images/change-sheet0.png",
+		"images/circle1-sheet0.png",
+		"images/touch1-sheet0.png",
+		"images/ball1-sheet0.png",
+		"media/bgm.ogg"
+	]
+}
